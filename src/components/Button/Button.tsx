72101cfb747d251147a02181987fae4f18c60d62
Reihane Blosenhauer <reihaneb@liome.fr>
2024-02-24 14:55:22 +0100

    Add Button component with styles and stories

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,41 @@
+import { memo } from 'react';
+
+import { useClickable } from '@/hooks/useClickable/useClickable';
+import { useFocusable } from '@/hooks/useFocusable/useFocusable';
+
+import styles from './Button.module.css';
+import type { ButtonProps } from './Button.d';
+
+function Button({
+  onClick,
+  children,
+  disabled = false,
+  disableDebounce = false,
+  autoFocus = false,
+  forwardedRef = null,
+  ...rest
+}: ButtonProps) {
+  const { eventHandlers } = useClickable({
+    onClick,
+    disabled,
+    disableDebounce,
+  });
+
+  const { ref: elementRef } = useFocusable(forwardedRef, { autoFocus });
+
+  return (
+    <button
+      type="button"
+      className={styles.rootStyle}
+      ref={elementRef}
+      disabled={disabled}
+      data-testid="button-component"
+      {...eventHandlers}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default memo(Button);
