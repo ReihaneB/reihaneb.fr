28c3ee3dcf01a9b97e68685349adeb88911c4f76
Reihane Blosenhauer <reihaneb@liome.fr>
2024-02-25 21:47:35 +0100

    Remove SVGR since next/image support svg

--- src/components/Link/Link.d.ts
+++ src/components/Link/Link.d.ts
@@ -1,8 +1,10 @@
 import React from 'react';
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 
 interface LinkProps {
   to: string;
   children: React.ReactNode;
+  Icon?: StaticImport | null;
   autoFocus?: boolean;
   forwardedRef?: React.RefObject<HTMLButtonElement> | null;
 }
