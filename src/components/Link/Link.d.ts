f64c9ff4e8da02e458529c0b81f049c592032788
ReihaneB <github-action@actions-template-sync.noreply.github.com>
2024-02-25 16:19:37 +0000

    chore(template): merge template changes :up:
    Signed-off-by: ReihaneB <github-action@actions-template-sync.noreply.github.com>

--- src/components/Link/Link.d.ts
+++ src/components/Link/Link.d.ts
@@ -3,7 +3,6 @@ import React from 'react';
 interface LinkProps {
   to: string;
   children: React.ReactNode;
-  Icon?: React.ElementType | null;
   autoFocus?: boolean;
   forwardedRef?: React.RefObject<HTMLButtonElement> | null;
 }
