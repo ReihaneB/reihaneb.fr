a254130e9148fb869112d38d699209bb0f695a1b
Reihane Blosenhauer <reihaneb@liome.fr>
2024-02-26 21:19:21 +0100

    Add getStrapiURL and getStrapiMedia functions to handle Strapi media URLs

---
+++ src/utils/strapiMedia.ts
@@ -0,0 +1,16 @@
+export function getStrapiURL(path = '') {
+  return `${process.env.API_URL || 'http://localhost:1337'}${path}`;
+}
+
+export function getStrapiMedia(url: string | null) {
+  if (url == null) {
+    return null;
+  }
+
+  // Return the full URL if the media is hosted on an external provider
+  if (url.startsWith('http') || url.startsWith('//')) {
+    return url;
+  }
+
+  return `${getStrapiURL()}${url}`;
+}
