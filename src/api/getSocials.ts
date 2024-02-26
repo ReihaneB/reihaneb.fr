205c72d1a6b05067acd2677745fb52f258d0536c
Reihane Blosenhauer <reihaneb@liome.fr>
2024-02-26 21:19:28 +0100

    Add getSocials API endpoint

---
+++ src/api/getSocials.ts
@@ -0,0 +1,12 @@
+import enhancedFetch from '@/utils/enhancedFetch/enhancedFetch';
+
+export const getSocials = async () => {
+  const { body } = await enhancedFetch({
+    method: 'GET',
+    path: '/social',
+    tag: 'socials',
+    state: 'dynamic',
+  });
+
+  return body;
+};
