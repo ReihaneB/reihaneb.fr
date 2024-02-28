e34c907cd6c5c1b8f9147274370fef89309f7667
Reihane Blosenhauer <reihaneb@liome.fr>
2024-02-28 18:10:35 +0100

    Refactor StackCategory component props

--- src/widgets/StackCategory/StackCategory.stories.tsx
+++ src/widgets/StackCategory/StackCategory.stories.tsx
@@ -13,33 +13,30 @@ export default meta;
 type Story = StoryObj<typeof StackCategory>;
 
 const defaultArgs = {
-  stackCategory:
+  id: 1,
+  name: 'Librairie',
+  tags: [
     {
       id: 1,
-      name: 'Librairie',
-      tags: [
-        {
-          id: 1,
-          label: 'React',
-          icon: IconArrow,
-        },
-        {
-          id: 2,
-          label: 'Redux',
-          icon: IconArrow,
-        },
-        {
-          id: 3,
-          label: 'Jest',
-          icon: IconArrow,
-        },
-        {
-          id: 4,
-          label: 'Next.js',
-          icon: IconArrow,
-        },
-      ],
+      label: 'React',
+      icon: IconArrow,
     },
+    {
+      id: 2,
+      label: 'Redux',
+      icon: IconArrow,
+    },
+    {
+      id: 3,
+      label: 'Jest',
+      icon: IconArrow,
+    },
+    {
+      id: 4,
+      label: 'Next.js',
+      icon: IconArrow,
+    },
+  ],
 };
 
 export const Playground: Story = {
@@ -51,75 +48,71 @@ export const Playground: Story = {
 export const WithLongName: Story = {
   args: {
     ...defaultArgs,
-    stackCategory: {
-      ...defaultArgs.stackCategory,
-      name: 'Duis adipisicing ipsum in exercitation ut tempor voluptate eiusmod adipisicing.',
-    },
+    ...defaultArgs,
+    name: 'Duis adipisicing ipsum in exercitation ut tempor voluptate eiusmod adipisicing.',
   },
 };
 
 export const WithLongTags: Story = {
   args: {
     ...defaultArgs,
-    stackCategory: {
-      ...defaultArgs.stackCategory,
-      tags: [
-        {
-          id: 1,
-          label: 'React',
-          icon: IconArrow,
-        },
-        {
-          id: 2,
-          label: 'Redux',
-          icon: IconArrow,
-        },
-        {
-          id: 3,
-          label: 'Jest',
-          icon: IconArrow,
-        },
-        {
-          id: 4,
-          icon: IconArrow,
-          label: 'Enzyme',
-        },
-        {
-          id: 5,
-          label: 'Webpack',
-          icon: IconArrow,
-        },
-        {
-          id: 6,
-          label: 'Babel',
-          icon: IconArrow,
-        },
-        {
-          id: 7,
-          label: 'ESLint',
-          icon: IconArrow,
-        },
-        {
-          id: 8,
-          label: 'Prettier',
-          icon: IconArrow,
-        },
-        {
-          id: 9,
-          label: 'TypeScript',
-          icon: IconArrow,
-        },
-        {
-          id: 10,
-          label: 'Jest',
-          icon: IconArrow,
-        },
-        {
-          id: 11,
-          label: 'Enzyme',
-          icon: IconArrow,
-        },
-      ],
-    },
+    ...defaultArgs,
+    tags: [
+      {
+        id: 1,
+        label: 'React',
+        icon: IconArrow,
+      },
+      {
+        id: 2,
+        label: 'Redux',
+        icon: IconArrow,
+      },
+      {
+        id: 3,
+        label: 'Jest',
+        icon: IconArrow,
+      },
+      {
+        id: 4,
+        icon: IconArrow,
+        label: 'Enzyme',
+      },
+      {
+        id: 5,
+        label: 'Webpack',
+        icon: IconArrow,
+      },
+      {
+        id: 6,
+        label: 'Babel',
+        icon: IconArrow,
+      },
+      {
+        id: 7,
+        label: 'ESLint',
+        icon: IconArrow,
+      },
+      {
+        id: 8,
+        label: 'Prettier',
+        icon: IconArrow,
+      },
+      {
+        id: 9,
+        label: 'TypeScript',
+        icon: IconArrow,
+      },
+      {
+        id: 10,
+        label: 'Jest',
+        icon: IconArrow,
+      },
+      {
+        id: 11,
+        label: 'Enzyme',
+        icon: IconArrow,
+      },
+    ],
   },
 };
