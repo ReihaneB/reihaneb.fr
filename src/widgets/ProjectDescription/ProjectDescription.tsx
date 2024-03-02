69fdf802778854ae29cd05852fe5377cd2e69f3f
Reihane Blosenhauer <reihaneb@liome.fr>
2024-03-02 11:26:51 +0100

    Add ProjectDescription snapshot

---
+++ src/widgets/ProjectDescription/ProjectDescription.tsx
@@ -0,0 +1,38 @@
+import Image from '@/components/Image/Image';
+
+import CompanyDescription from '../CompanyDescription/CompanyDescription';
+import ObjectiveDescription from '../ObjectiveDescription/ObjectiveDescription';
+
+import type { ProjectDescriptionProps } from './ProjectDescription.d';
+import styles from './ProjectDescription.module.css';
+
+function ProjectDescription({
+  companyDescription,
+  objective,
+  ...rest
+}: ProjectDescriptionProps) {
+  return (
+    <div
+      className={styles.root}
+      {...rest}
+    >
+      <div className={styles.company}>
+        <CompanyDescription description={companyDescription} />
+      </div>
+      <div className={styles.objective}>
+        <ObjectiveDescription
+          title={objective.title}
+          description={objective.description}
+        />
+      </div>
+      <div className={styles.image}>
+        <Image
+          src={objective.image.src}
+          alt={objective.image.alt}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ProjectDescription;
