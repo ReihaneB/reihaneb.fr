c8870fb7a337950d6ea35b5122e7c88c3b03ce1e
Reihane Blosenhauer <reihaneb@liome.fr>
2024-03-03 17:16:08 +0100

    Add HeroHeader with styles and stories

---
+++ src/widgets/HeroHeader/HeroHeader.stories.tsx
@@ -0,0 +1,122 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import IconArrow from '@/styles/icons/arrows/arrow.svg';
+import _1x1 from '@/styles/assets/placeholder/_1x1.png';
+import _16x9 from '@/styles/assets/placeholder/_16x9.png';
+import _4x3 from '@/styles/assets/placeholder/_4x3.png';
+
+import HeroHeader from './HeroHeader';
+
+import styles from './HeroHeader.stories.module.css';
+
+const meta: Meta<typeof HeroHeader> = {
+  title: 'Widgets/HeroHeader',
+  component: HeroHeader,
+  decorators: [
+    Story => (
+      <div className={styles.root}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof HeroHeader>;
+
+const defaultArgs = {
+  name: 'John Doe',
+  title: 'Software Engineer',
+  description: {
+    title: 'About me',
+    content: 'I am a software engineer with a passion for web development.',
+  },
+  image: {
+    src: _1x1,
+    alt: 'John Doe',
+  },
+  socials: [
+    {
+      name: 'GitHub',
+      url: '',
+      icon: IconArrow,
+    },
+    {
+      name: 'Twitter',
+      url: '',
+      icon: IconArrow,
+    },
+  ],
+};
+
+export const Playground: Story = {
+  args: {
+    ...defaultArgs,
+  },
+};
+
+export const LongName: Story = {
+  args: {
+    ...defaultArgs,
+    name: 'Magna aute ex ipsum fugiat velit sunt aliqua enim qui sint culpa sint.',
+  },
+};
+
+export const LongTitle: Story = {
+  args: {
+    ...defaultArgs,
+    title: 'Magna aute ex ipsum fugiat velit sunt aliqua enim qui sint culpa sint.',
+  },
+};
+
+export const LongDescription: Story = {
+  args: {
+    ...defaultArgs,
+    description: {
+      title: 'Magna aute ex ipsum fugiat velit sunt aliqua enim qui sint culpa sint.',
+      content: 'Enim ea labore excepteur excepteur est nostrud aliquip veniam ullamco. Elit incididunt aute dolor officia aute veniam amet pariatur veniam laborum. Eu est tempor amet nulla non excepteur occaecat non cupidatat. Ut aliquip elit dolore culpa dolore eiusmod excepteur tempor veniam id adipisicing ipsum commodo. Dolor sit eu magna duis veniam do laboris laboris esse excepteur officia enim pariatur. Deserunt veniam aliqua in laborum. Cupidatat occaecat laboris incididunt consectetur do est eu cillum non elit fugiat qui mollit.',
+    },
+  },
+};
+
+export const LongData: Story = {
+  args: {
+    ...defaultArgs,
+    name: 'Magna aute ex ipsum fugiat velit sunt aliqua enim qui sint culpa sint.',
+    title: 'Magna aute ex ipsum fugiat velit sunt aliqua enim qui sint culpa sint.',
+    description: {
+      title: 'Velit consequat sint consectetur tempor irure eu. Anim minim aliquip mollit nisi pariatur ea do aute duis sunt exercitation laboris culpa. Excepteur ut ad est laborum et laboris. Laborum sunt ad reprehenderit sunt ullamco esse voluptate reprehenderit. Sunt anim Lorem enim nisi adipisicing aute minim voluptate nostrud labore. Ullamco eu pariatur eu esse aute occaecat ex nostrud aliqua ex nulla consequat non.',
+      content: 'Enim ea labore excepteur excepteur est nostrud aliquip veniam ullamco. Elit incididunt aute dolor officia aute veniam amet pariatur veniam laborum. Eu est tempor amet nulla non excepteur occaecat non cupidatat. Ut aliquip elit dolore culpa dolore eiusmod excepteur tempor veniam id adipisicing ipsum commodo. Dolor sit eu magna duis veniam do laboris laboris esse excepteur officia enim pariatur. Deserunt veniam aliqua in laborum. Cupidatat occaecat laboris incididunt consectetur do est eu cillum non elit fugiat qui mollit.',
+    },
+  },
+};
+
+export const _1x1Image: Story = {
+  args: {
+    ...defaultArgs,
+    image: {
+      src: _1x1,
+      alt: 'John Doe',
+    },
+  },
+};
+
+export const _16x9Image: Story = {
+  args: {
+    ...defaultArgs,
+    image: {
+      src: _16x9,
+      alt: 'John Doe',
+    },
+  },
+};
+
+export const _4x3Image: Story = {
+  args: {
+    ...defaultArgs,
+    image: {
+      src: _4x3,
+      alt: 'John Doe',
+    },
+  },
+};
---
+++ src/widgets/HeroHeader/HeroHeader.tsx
@@ -0,0 +1,49 @@
+import Image from '@/components/Image/Image';
+
+import Socials from '../Socials/Socials';
+
+import type { HeroHeaderProps } from './HeroHeader.d';
+import styles from './HeroHeader.module.css';
+
+function HeroHeader({
+  name,
+  title,
+  description,
+  image,
+  socials,
+  ...rest
+}: HeroHeaderProps) {
+  return (
+    <div
+      className={styles.root}
+      {...rest}
+    >
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>
+          {title}
+        </h1>
+        <h2 className={styles.name}>
+          {name}
+        </h2>
+      </div>
+      <div className={styles.image}>
+        <Image
+          src={image.src}
+          alt={image.alt}
+        />
+      </div>
+      <div className={styles.content}>
+        <Socials
+          additionalClasses={styles.socials}
+          socials={socials}
+        />
+        <h3 className={styles.description}>
+          {description.title}
+        </h3>
+        <p>{description.content}</p>
+      </div>
+    </div>
+  );
+}
+
+export default HeroHeader;
