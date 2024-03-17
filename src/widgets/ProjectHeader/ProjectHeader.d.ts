f0c096719e139ecead8376e8144064fd820b41b8
Reihane Blosenhauer <reihaneb@liome.fr>
2024-03-17 18:30:16 +0100

    Add id field to ProjectsList interface

--- src/widgets/ProjectHeader/ProjectHeader.d.ts
+++ src/widgets/ProjectHeader/ProjectHeader.d.ts
@@ -3,6 +3,7 @@ import { StaticImageData } from 'next/image';
 import type { Stack } from '@/widgets/MainStack/MainStack.d';
 
 export interface Project {
+  id?: number;
   title: string;
   jobTitle: string;
   stack: Stack[];
