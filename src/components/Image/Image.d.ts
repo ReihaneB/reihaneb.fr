55abdb198b1f52153df78f05a730cc2c24ce2268
Reihane Blosenhauer <reihaneb@liome.fr>
2024-03-17 17:31:26 +0100

    Add getProjectDescription API and ProjectDescriptionStore

--- src/components/Image/Image.d.ts
+++ src/components/Image/Image.d.ts
@@ -1,6 +1,6 @@
 import { ImageProps as NextImageProps, StaticImageData } from 'next/image';
 
 export interface ImageProps extends NextImageProps {
-  src: StaticImageData;
+  src: StaticImageData | string;
   alt: string;
 }
