4ac051c73e79cbb460be747e61da5c3058bc7288
Reihane Blosenhauer <reihaneb@liome.fr>
2024-03-17 18:17:39 +0100

    Refactor getStrapiMedia function to remove null check

--- src/utils/strapiMedia.ts
+++ src/utils/strapiMedia.ts
@@ -2,11 +2,7 @@ export function getStrapiURL(path = '') {
   return `${process.env.API_URL || 'http://localhost:1337'}${path}`;
 }
 
-export function getStrapiMedia(url: string | null) {
-  if (url == null) {
-    return null;
-  }
-
+export function getStrapiMedia(url: string) {
   // Return the full URL if the media is hosted on an external provider
   if (url.startsWith('http') || url.startsWith('//')) {
     return url;
