ce3dee2237879925547bbfc633bdcd9721877378
Reihane Blosenhauer <reihaneb@liome.fr>
2024-04-25 18:04:03 +0200

    Implementation of SEO

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,30 @@
+import { Metadata } from 'next';
+
 import Home from '@/pageComponents/Home/Home';
+import { getAboutMe } from '@/api/getAboutMe';
+
+export async function generateMetadata(): Promise<Metadata> {
+  const about = await getAboutMe();
+
+  return {
+    title: `${about.jobTitle} • ${about.name}`,
+    description: `${about.description}`,
+    openGraph: {
+      title: `${about.jobTitle} • ${about.name}`,
+      description: `${about.description}`,
+      type: 'website',
+      url: 'https://www.reihaneb.fr',
+      images: [
+        {
+          url: about.image.src,
+          width: about.image.width,
+          height: about.image.height,
+          alt: about.image.alt,
+        },
+      ],
+    },
+  };
+}
 
 function HomePage() {
   return (
